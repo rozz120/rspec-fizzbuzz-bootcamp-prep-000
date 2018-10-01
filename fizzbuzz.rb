edffe30b1a7a213b4f4212605ed43c8901bc8fa8
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-01 10:18:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,18 @@
 
 
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number 
+  
+  
+  elsif number % 3 == 0
     
     "Fizz"
   
 elsif number % 5 == 0
     "Buzz"
+    
+    
+    
+    
   end
 end
